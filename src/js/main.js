ebd39478beefd13bd254b4fb1edc0e935e5570d6
ElenoreGill <elenoregill55@gmail.com>
2023-05-11 08:51:33 +0200

    carousel - loop et slides per view

--- src/js/main.js
+++ src/js/main.js
@@ -3,21 +3,33 @@ import Swiper, { Navigation, Pagination } from "swiper";
 Swiper.use([Navigation, Pagination]);
 
 const swiper = new Swiper(".swiper-container", {
+  clickable: true, // cssMode: true,
+
+  loop: true,
+
+  speed: 1000,
+
+  mousewheel: true,
+
+  keyboard: true,
+
+  spaceBetween: 50,
+
   pagination: {
     el: ".swiper-pagination",
   },
+
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-pagination",
+
+    prevEl: ".swiper-pagination",
   },
-});
-<script>
-var swiper = new Swiper(".mySwiper", {
+
+  autoplay: {
+    delay: 2500,
+
+    disableOnInteraction: false,
+  },
   slidesPerView: 3,
-  spaceBetween: 0,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  spaceBetween: 20,
 });
-</script>
